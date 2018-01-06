b1ded8e6f114363d27fb76dd6fba70ac154783f6
Robin Lu <lqlsoft@hotmail.com>
2018-01-06 13:49:00 +0800

    Using parameters & Add result judgement
    Using parameters to get username&password.
    Add result judgement to judge if login.

--- shell/login.sh
+++ shell/login.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Your StuNo. & Password
-username='00000000'
-password='00000000'
+# Using Parameters
+# Use: ./login.sh username password
+username=$1
+password=$2
 curl --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.66 --silent -o LOGIN_STATUS.out
-echo "Login Success!!"
+if [[ "grep UID='${username}' LOGIN_STATUS.out" != "" ]]
+then
+echo "Login Success"
+else
+echo "Login Fail"
+fi
