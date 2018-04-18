66d1f6a2f07332b91084f139589c6770afeb7026
Robin Lu <lqlsoft@hotmail.com>
2018-04-18 10:17:27 +0800

    bug fix
    emmmmm

--- shell/login.sh
+++ shell/login.sh
@@ -5,11 +5,16 @@
 username=$1
 password=$2
 curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.66 --silent > LOGIN_STATUS.out
-curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.82 --silent > LOGIN_STATUS.out
 if [[ "grep UID='${username}' LOGIN_STATUS.out" != "" ]]
 then
   echo "Login Success"
 else
-  echo "Login Fail"
+  curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.82 --silent > LOGIN_STATUS.out
+  if [[ "grep UID='${username}' LOGIN_STATUS.out" != "" ]]
+  then
+    echo "Login Success"
+  else
+    echo "Login Fail"
+  fi
 fi
 rm -f LOGIN_STATUS.out
