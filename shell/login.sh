c8369424dad23af250af1de4a0817c189dbac9df
Robin Lu <lqlsoft@hotmail.com>
2018-04-18 10:14:26 +0800

    add max wait-time && two ways to login.

--- shell/login.sh
+++ shell/login.sh
@@ -4,10 +4,12 @@
 # Use: ./login.sh username password
 username=$1
 password=$2
-curl --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.66 --silent -o LOGIN_STATUS.out
+curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.66 --silent > LOGIN_STATUS.out
+curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.82 --silent > LOGIN_STATUS.out
 if [[ "grep UID='${username}' LOGIN_STATUS.out" != "" ]]
 then
-echo "Login Success"
+  echo "Login Success"
 else
-echo "Login Fail"
+  echo "Login Fail"
 fi
+rm -f LOGIN_STATUS.out
