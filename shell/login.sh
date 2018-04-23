de1eda886ff55ee613d49fec074dfebad5f3eb9f
Jeason <zhaojitong@xiaomi.com>
2018-04-23 18:43:29 +0800

    Fix

--- shell/login.sh
+++ shell/login.sh
@@ -4,12 +4,12 @@
 # Use: ./login.sh username password
 username=$1
 password=$2
-res=`curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.66 --silent | grep UID=${username}`
-if [res != "" ]
+res=`curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.66 --silent | grep UID=\'${username}`
+if [ res != "" ]
 then
   echo "Login Success"
 else
-  res = `curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.82 --silent | grep UID=${username}`
+  res = `curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.82 --silent | grep UID=\'${username}`
   if [[ res != "" ]]
   then
     echo "Login Success"
