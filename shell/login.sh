d9e1ca1afca8919e527352d873cb9d95dcc17ed4
Robin Lu <lqlsoft@hotmail.com>
2018-04-23 17:51:50 +0800

    fetch msg from stdout
    script will not generate a .out file any more

--- shell/login.sh
+++ shell/login.sh
@@ -4,17 +4,16 @@
 # Use: ./login.sh username password
 username=$1
 password=$2
-curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.66 --silent > LOGIN_STATUS.out
-if [[ "grep UID='${username}' LOGIN_STATUS.out" != "" ]]
+res=`curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.66 --silent | grep UID=${username}`
+if [res != "" ]
 then
   echo "Login Success"
 else
-  curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.82 --silent > LOGIN_STATUS.out
-  if [[ "grep UID='${username}' LOGIN_STATUS.out" != "" ]]
+  res = `curl --max-time 1 --retry 3 --data  "DDDDD=${username}&upass=${password}&0MKKey=123456789" http://202.204.48.82 --silent | grep UID=${username}`
+  if [[ res != "" ]]
   then
     echo "Login Success"
   else
     echo "Login Fail"
   fi
 fi
-rm -f LOGIN_STATUS.out
